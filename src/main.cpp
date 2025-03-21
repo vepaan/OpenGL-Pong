d011d90bb916050dec1cf56a9012f05675d6ad5c
vepaan <141347566+vepaan@users.noreply.github.com>
2025-03-20 22:20:52 -0500

    setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,49 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-using namespace std; 
+using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-int main() {
-    if (!glfwInit()) {
-        cout << "Failed to initialize GLFW" << endl;
-        return -1;
-    }
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
-    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor()); 
-
-    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
-
-    glfwWindowHint(GLFW_SAMPLES, 4);
+int main()
+{
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "PingPong", NULL, NULL);
+    int WIDTH = 800;
+    int HEIGHT = 600;
 
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ping Pong", NULL, NULL);
     if (window == NULL) {
-        cout << "Failed to open GLFW window" << endl;
+        cout << "Failed to initialize window" << endl;
+        glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        cout << "Failed to initialize GLAD" << endl;
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    while(!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
-        glfwPollEvents();    
+        glfwPollEvents(); 
     }
 
     glfwTerminate();
