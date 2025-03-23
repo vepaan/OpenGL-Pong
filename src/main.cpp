fe642ee19f3313cbdb4fdbc4335741575fafd7a1
vepaan <141347566+vepaan@users.noreply.github.com>
2025-03-22 22:09:30 -0500

    built rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,49 @@ void processInput(GLFWwindow *window) {
         glfwSetWindowShouldClose(window, true);
 }
 
+void triangleSetup(unsigned int VAO, unsigned int VBO) {
+    // Intialize triangle vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // Initialization code (done once unless your object frequently changes)
+    // bind VAO
+    glBindVertexArray(VAO);
+    // copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // then set our vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+void rectangleSetup(unsigned int VAO, unsigned int VBO, unsigned int EBO) {
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0); 
+}
+
 int main()
 {
     glfwInit();
@@ -62,30 +105,18 @@ int main()
 
     glUseProgram(shaderProgram);
 
-    // Initialize buffer object and array object
+    // Initialize buffer object, array object and element object
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 
     unsigned int VAO;
-    glGenVertexArrays(1, &VAO);  
+    glGenVertexArrays(1, &VAO);
 
-    // Intialize triangle vertices
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
-    // Initialization code (done once unless your object frequently changes)
-    // bind VAO
-    glBindVertexArray(VAO);
-    // copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
+    //triangleSetup(VAO, VBO);
+    rectangleSetup(VAO, VBO, EBO);
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -94,7 +125,9 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents(); 
